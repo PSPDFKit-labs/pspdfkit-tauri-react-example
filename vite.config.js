c25f821f43e9b5bdd45b68bc78bd056df3c0d1ef
Guillermo Peralta Scura <gperaltascura@gmail.com>
2022-09-23 15:54:12 -0400

    Dynamically copy PSPDFKit assets during build process

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 /** @type {import('vite').UserConfig} */
-import { defineConfig } from 'vite';
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
-    server: {
-        port: 3000
-    },
-    plugins: [react()],
-    build: {
-        outDir: "build"
-    }
-})
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        {
+          src: "node_modules/pspdfkit/dist/pspdfkit-lib",
+          dest: "public/",
+        },
+      ],
+      hook: "buildStart",
+    }),
+  ],
+  build: {
+    outDir: "build",
+  },
+});
